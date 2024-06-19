69864b9445967aacb79b779a08ce13b2dafc5a7d
João Paulo <joaowais@gmail.com>
2024-06-19 17:42:52 -0300

    Aula 01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        File file = new File("E:\\DEVELOPMENT\\Java\\Projetos\\arquivo.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
     }
 }
