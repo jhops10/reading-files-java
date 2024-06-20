37330310af8b883e3ce812f3113d390987ccba8c
João Paulo <joaowais@gmail.com>
2024-06-19 23:33:16 -0300

    Aula 03

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,17 @@ public class Main {
     public static void main(String[] args)  {
        String path = "E:\\DEVELOPMENT\\Java\\Projetos\\arquivo.txt";
 
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null){
                 System.out.println(line);
                 line = br.readLine();
             }
-
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        finally {
-            try {
-                if(fr != null){
-                    fr.close();
-                }
-                if(br != null){
-                    br.close();
-                }
-            } catch (IOException e){
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
+
     }
 }
